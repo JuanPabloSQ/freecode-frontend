836345e610f05085b0c7a3157cddaa37e256e6b4
JP Sherka <jp.sherka@gmail.com>
2024-06-02 20:54:06 -0400

    fixed app problem

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,51 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { createHashRouter, RouterProvider } from 'react-router-dom';
 import { ThemeContextProvider } from './context/ThemeContext';
 import Page from './page/Page';
-import QuoteMachinePage from "./page/QuoteMachinePage"
-import CalculatorPage from "./page/CalculatorPage"
-import ClockPage from "./page/ClockPage"
+import QuoteMachinePage from "./page/QuoteMachinePage";
+import CalculatorPage from "./page/CalculatorPage";
+import ClockPage from "./page/ClockPage";
 import DrumMachinePage from './page/DrumMachinePage';
 import MarkdownPage from './page/MarkdownPage';
+
+const router = createHashRouter(
+  [
+    {
+      path: "/",
+      children: [
+        {
+          path: "freecode-frontend/",
+          element: <Page />,
+        },
+        {
+          path: "freecode-frontend/calculator",
+          element: <CalculatorPage />,
+        },
+        {
+          path: "freecode-frontend/clock",
+          element: <ClockPage />,
+        },
+        {
+          path: "freecode-frontend/quote-machine",
+          element: <QuoteMachinePage />,
+        },
+        {
+          path: "freecode-frontend/drum-machine",
+          element: <DrumMachinePage />,
+        },
+        {
+          path: "freecode-frontend/markdown",
+          element: <MarkdownPage />,
+        },
+      ],
+    },
+  ]
+);
 
 function App() {
   return (
     <ThemeContextProvider>
-      <Router>
-        <Routes>
-          <Route path="/freecode-frontend/" element={<Page />} />
-          <Route path="/freecode-frontend/calculator" element={<CalculatorPage />} />
-          <Route path="/freecode-frontend/clock" element={<ClockPage />} />
-          <Route path="/freecode-frontend/quote-machine" element={<QuoteMachinePage />} />
-          <Route path="/freecode-frontend/drum-machine" element={<DrumMachinePage />} /> 
-          <Route path="/freecode-frontend/markdown" element={<MarkdownPage />} /> 
-        </Routes>
-      </Router>
+      <RouterProvider router={router} />
     </ThemeContextProvider>
   );
 }
